4b0f0a6ac453743bfb0db64d2e66aae87e64ea7e
cristiancappelletti <cristiancappelletti@mbp-di-cristian>
2020-03-24 11:10:46 +0100

    Siamo in remoto

--- GitExample/src/base/Exemple1.java
+++ GitExample/src/base/Exemple1.java
@@ -7,7 +7,8 @@ public class Exemple1 {
 	public static void main(String[] args) {
 		System.out.println(metodo1());
 		Example2 e2=new Example2();
-		System.out.println(e2.metodo2("ciao","addio"));
+		System.out.println(e2.metodo2("ciao","prova"));
+		System.out.println(e2.metodo2("ciao","remoto"));
 		
 
 	}
