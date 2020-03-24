07b4ebd42789b92346866ff1f9ad601eb7122c28
cristiancappelletti <cristiancappelletti@mbp-di-cristian>
2020-03-24 10:06:03 +0100

    Aggiunta di una classe esterna

---
+++ GitExample/src/avanzato/Example2.java
@@ -0,0 +1,7 @@
+package avanzato;
+
+public class Example2 {
+	public String metodo2(String a,String b) {
+		return a+" "+b;
+	}
+}
--- GitExample/src/base/Exemple1.java
+++ GitExample/src/base/Exemple1.java
@@ -1,9 +1,14 @@
 package base;
 
+import avanzato.Example2;
+
 public class Exemple1 {
 
 	public static void main(String[] args) {
 		System.out.println(metodo1());
+		Example2 e2=new Example2();
+		System.out.println(e2.metodo2("ciao","addio"));
+		
 
 	}
 	public static String metodo1() {
